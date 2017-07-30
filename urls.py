44b4d56f606ed716739c1f8d21039a62a42ffd28
Armenvardanyan95 <armenvardanyan95@gmail.com>
2017-07-30 22:10:21 +0400

    Add files

---
+++ urls.py
@@ -0,0 +1,36 @@
+"""MapPro URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from MapPro.accounts.views import UserViewSet
+from MapPro.orders.views import ColorMarkerViewSet, OrderViewSet
+
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+
+router = DefaultRouter()
+
+router.register('users', UserViewSet)
+router.register('colors', ColorMarkerViewSet)
+router.register('orders', OrderViewSet)
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', views.obtain_auth_token),
+]
